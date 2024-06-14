c60341a51375d82e4072079a75e98b5d31dc90a0
Iyanu Henry famoritiye <henry.famoritiye@gmail.com>
2024-06-14 05:19:21 +0100

    second commit

--- src/assets/pages/contact/Contacts.jsx
+++ src/assets/pages/contact/Contacts.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Project from "../project/Project";
 
 const Contacts = () => {
   return (
     <div>
-      <h1>CONTACTS</h1>
+      <Project />
     </div>
   );
 };
--- src/assets/pages/project/Project.jsx
+++ src/assets/pages/project/Project.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import Mainheader from "../header/Mainheader";
 import Footer from "../main/Footer";
+import styled from "styled-components";
 const Project = () => {
+  const Container = styled.div`
+    max-width: 130rem;
+    padding: 0 4.2rem;
+    margin: 0 auto;
+    font-family: "roboto", sans-serif;
+  `;
+  const Span = styled.span`
+    font-size: 24px;
+    font-weight: 300;
+    color: #212529;
+    font-family: roboto, sans-serif;
+    display: flex;
+    margin-top: 200px;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 400px;
+  `;
   return (
-    <div>
+    <Container>
       <Mainheader />
 
-      <h1>PROJECTS</h1>
+      <Span>no page found</Span>
       <Footer />
-    </div>
+    </Container>
   );
 };
 export default Project;
